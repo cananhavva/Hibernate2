2f076f6b25768cc227a85131fab2ee80780e74ec
cananhavva <cananhavvaarslan@gmail.com>
2021-12-16 20:19:56 +0300

    hibernate inheritance

---
+++ src/main/java/com/bilgeadam/util/HibernateUtil.java
@@ -0,0 +1,39 @@
+package com.bilgeadam.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.bilgeadam.entity.CityEntity;
+import com.bilgeadam.entity.MsiEntity;
+
+import embedded_embeddable.PersonEntity;
+
+public class HibernateUtil {
+	// dış dünyada bununla bu classa erişim sağlayabileceğim.
+	public static SessionFactory getSessionfactory() {
+		return sessionFactory;
+	}
+	
+	// hibernate ile bağlantı kuracağım yer
+	private static final SessionFactory sessionFactory = sessionFactoryHibernate();
+	
+	// method
+	private static SessionFactory sessionFactoryHibernate() {
+		try {
+			// instance
+			Configuration configuration = new Configuration();
+			
+			// entity classlarımızı buraya ekleyeceğiz
+			configuration.addAnnotatedClass(CityEntity.class);
+			configuration.addAnnotatedClass(MsiEntity.class); // superclasstan aldık
+			configuration.addAnnotatedClass(PersonEntity.class); // superclasstan aldık
+			
+			SessionFactory factory = configuration.configure("hibernate.cfg.xml").buildSessionFactory();
+			return factory;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
